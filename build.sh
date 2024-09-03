dd1f4e0ba10a2e59a27bc0eda8abc1334a76b1fb
kiruse <26940742+Kiruse@users.noreply.github.com>
2024-09-03 23:59:01 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 dir=$(realpath $(dirname $0))
 
+cd "$dir/packages/core"
+
+# Clean all packages
 for package in $(printf '%s ' $dir/packages/*); do
   cd $package
-  rm -rf dist
+  name=$(jq -r .name package.json)
+  echo "Cleaning package $name"
+  npm run clean
+done
+
+# Return to core package directory
+cd "$dir/packages/core"
+
+npm run build
+
+for package in $(printf '%s ' $dir/packages/*); do
+  if [ "$package" = "$dir/packages/core" ]; then
+    continue
+  fi
+
+  cd $package
   name=$(jq -r .name package.json)
   echo "Building package $name"
-  npm run clean
   npm run build
 done
