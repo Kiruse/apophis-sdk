e36146a2ba74a22442b803d480d8f3065ad2ded6
kiruse <26940742+Kiruse@users.noreply.github.com>
2025-05-20 08:36:42 +0200

    update Bank.Send to use hiproto + new Bank.Query.denomMetadata

--- packages/cosmos/src/msg/bank.ts
+++ packages/cosmos/src/msg/bank.ts
@@ -1,32 +1,53 @@
-import { registerDefaultProtobufs } from '@apophis-sdk/core/encoding/protobuf/any.js';
-import { Coin } from '@apophis-sdk/core/types.sdk.js';
-import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx';
+import { CosmosNetworkConfig } from '@apophis-sdk/core';
+import { registerDefaultProtobufSchema } from '@apophis-sdk/core/encoding/protobuf/any.js';
+import { hpb } from '@kiruse/hiproto';
+import { Cosmos } from '../api';
 import { registerDefaultAminos } from '../encoding/amino';
-import { TxMarshaller } from '../tx';
+import { pbCoin } from '../encoding/protobuf/core';
 
 export namespace Bank {
-  export type SendData = {
-    fromAddress: string;
-    toAddress: string;
-    amount: Coin[];
-  }
+  export type DenomMetadata = hpb.infer<typeof Query.pbMetadata>;
+
+  //#region Send
+  export type SendData = hpb.infer<typeof pbSendData>;
+
+  export const pbSendData = hpb.message({
+    fromAddress: hpb.string(1),
+    toAddress: hpb.string(2),
+    amount: hpb.repeated.submessage(3, pbCoin),
+  });
 
   export class Send {
-    static readonly protobufTypeUrl = '/cosmos.bank.v1beta1.MsgSend';
     static readonly aminoTypeUrl = 'cosmos-sdk/MsgSend';
-
+    static readonly protobufTypeUrl = '/cosmos.bank.v1beta1.MsgSend';
+    static readonly protobufSchema = pbSendData;
     constructor(public data: SendData) {}
-
-    static toProtobuf(value: Send): Uint8Array {
-      return MsgSend.encode(MsgSend.fromPartial(TxMarshaller.marshal(value.data) as any)).finish();
-    }
-
-    static fromProtobuf(value: Uint8Array): Send {
-      const { fromAddress, toAddress, amount } = TxMarshaller.unmarshal(MsgSend.decode(value)) as SendData;
-      return new Send({ fromAddress, toAddress, amount });
-    }
   };
 
-  registerDefaultProtobufs(Send);
+  registerDefaultProtobufSchema(Send);
   registerDefaultAminos(Send);
+  //#endregion
+
+  export namespace Query {
+    //#region DenomMetadata
+    export const pbMetadata = hpb.message({
+      description: hpb.string(1),
+      denomUnits: hpb.repeated.submessage(2, {
+        denom: hpb.string(1),
+        exponent: hpb.uint32(2),
+        aliases: hpb.repeated.string(3),
+      }),
+      base: hpb.string(3),
+      display: hpb.string(4),
+      name: hpb.string(5),
+      symbol: hpb.string(6),
+      uri: hpb.string(7),
+      uriHash: hpb.string(8),
+    });
+
+    export async function getDenomMetadata(network: CosmosNetworkConfig, denom: string) {
+      return await Cosmos.rest(network).cosmos.bank.v1beta1.denoms_metadata[denom]('GET');
+    }
+    //#endregion
+  }
 }
