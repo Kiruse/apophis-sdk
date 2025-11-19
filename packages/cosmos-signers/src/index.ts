2daad6bb8a7985da0629f5789c42d84dfef90c12
kiruse <26940742+Kiruse@users.noreply.github.com>
2025-11-19 17:06:01 +0100

    fix: Removed leak from feature branch

--- packages/cosmos-signers/src/index.ts
+++ packages/cosmos-signers/src/index.ts
@@ -1,16 +1,13 @@
 import { Signer } from '@apophis-sdk/core';
 import { Keplr, Leap } from '@apophis-sdk/keplr-signer';
 import { WalletConnectCosmosSigner } from '@apophis-sdk/walletconnect-signer';
-import { GalaxyStation } from './galaxystation.js';
 
 export * from '@apophis-sdk/keplr-signer';
 export * from '@apophis-sdk/walletconnect-signer';
-export * from './galaxystation.js';
 
 export function registerCosmosSigners(walletConnectProjectId?: string) {
   Signer.register(Keplr);
   Signer.register(Leap);
-  Signer.register(GalaxyStation);
   if (walletConnectProjectId) {
     Signer.register(new WalletConnectCosmosSigner({ projectId: walletConnectProjectId }));
   }
